01dba2c6d3abadfa497894d21ad84ad7b448401c
NiLPump <nil.casabella@mataro.epiaedu.cat>
2022-03-28 15:13:02 +0200

    un poco de sudo su

--- src/jocdelavida.java
+++ src/jocdelavida.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 public class jocdelavida {
+    Scanner e = new Scanner(System.in);
+    int i, j;
+    int vida;
 
     public static void main(String[] args) {
         jocdelavida p = new jocdelavida();
@@ -8,30 +11,82 @@ public class jocdelavida {
     }
 
     public void principal() {
-        int[][] array = new int[10][10];
+        boolean fi = false;
+        System.out.println("Introdueix les files: ");
+        i = e.nextInt();
+        System.out.println("Introdueix les columnes: ");
+        j = e.nextInt();
+        int [][]array = new int [i][j];
 
+
+        do {
+            System.out.println();
+            System.out.println();
+            System.out.println("Tria una opció: ");
+            System.out.println();
+            System.out.println("Manual [1] ");
+            System.out.println("Automàtic [2]");
+            System.out.println("Imprimir taula [3]");
+            System.out.println("Sortir [4] ");
+            int opcio = e.nextInt();
+            switch (opcio){
+                case 1: manual(vida, array);
+                break;
+                case 2: automatic(vida, array);
+                break;
+                case 3: imprimirArray(array);
+                break;
+                case 4: fi = true;
+                break;
+            }
+
+        }while (!fi==true);
     }
 
-    public void insertarCelula(int celula, int [][]array){
-        Scanner e = new Scanner(System.in);
-        System.out.println("Selecciona una fila 1-10:");//Seleccionar fila
-        i = e.nextInt();
-        i = (i-1);
-        System.out.println("Selecciona una columna 1-10");//Seleccionar colummna
-        j = e.nextInt();
-        j = (j-1);
-        if (i > 10 || j > 10){
-            System.out.println("Estas fora dels marges, selecciona una altre fila i columna dintre dels marges (10f, 10c!)");//Comprobar fila/columna
+    public void imprimirArray(int  [][]array){
+        for(int i = 0; i < array.length; i++){
+            for(int j = 0; j < array.length; j++){
+                System.out.print(array[i][j]);
+            }
+            System.out.println();
         }
-        else if (i <= 10 && j <=10 ) {
-            if (array[i][j]>0){
-                System.out.println("Ja hi ha un numero en aquesta casella! Selecciona una altre!");
+    }
 
-            }else if (j[i][j]==0){//dipositar numero
-                System.out.println("Quin numero vols dipositar?");
-                int numero = e.nextInt();
-                j[i][j]= numero;
+
+    public void manual (int vida, int [][]array){
+        int a = 5;
+        for (a=0; a<5; a++){
+            System.out.println("Introdueix una fila: ");
+            i = e.nextInt();
+            i = (i - 1);
+            System.out.println("Introdueix una columna: ");
+            j = e.nextInt();
+            j = (j - 1);
+            if (i > array.length || j > array.length) {
+                System.out.println("ERROR: Fora dels limits ");
+            } else if (i <= array.length && j <= array.length){
+                if (array[i][j] > 0) {
+                    System.out.println("ERROR: Ja hi ha un celula en aquesta posició ");
+                } else if (array[i][j] == 0) {
+                    System.out.println("Introdueix un numero: ");
+                    vida = e.nextInt();
+                    array[i][j] = vida;
+                }
             }
         }
+    }
+
+    public void automatic (int vida, int [][]array){
+        int a = 5;
+        for (a = 0; a < 5; a++){
+            i = (int)(Math.random()*array.length);
+            j = (int)(Math.random()*array[i].length);
+            vida = 1;
+            array[i][j] = vida;
+        }
+    }
+
+    public void jugar (int [][]array, int [][]array2){
+
     }
 }
